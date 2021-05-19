a0a4f53f7c3228829c0b83ea88fd2b80df16f644
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2021-05-18 23:23:39 -0300

    Initial commit

---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+        default: '#f5f6f7'
+    }
+  },
+});
+
+export default theme;
